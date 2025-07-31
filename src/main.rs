335e57d95ef084b3f7d1ecb684b85f1ee158ec45
NickKhalow <nickhalow@gmail.com>
2025-08-01 01:24:20 +0300

    subscribe base

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
-use livekit::{Room, RoomOptions};
+use livekit::{track::RemoteTrack, webrtc::audio_stream::native::NativeAudioStream, Room, RoomEvent, RoomOptions};
 
-async fn main() -> anyhow::Result {
-    let url = std::env!("LIVEKIT_URL");
-    let token = std::env!("LIVEKIT_TOKEN");
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let url = std::env::var("LIVEKIT_URL")?;
+    let token = std::env::var("LIVEKIT_TOKEN")?;
     let options = RoomOptions::default();
 
-    let (room, events) = Room::connect(url, token, options).await?;
+    let (room, mut events) = Room::connect(&url, &token, options).await?;
 
-    if let Some(event: RoomEvent) = events.recv() {
-        
+    loop {
+        if let Some(event) = events.recv().await {
+            match event {
+                RoomEvent::TrackPublished {
+                    publication,
+                    participant,
+                } => {
+
+
+                },
+                RoomEvent::TrackSubscribed { track, publication, participant } => {
+                    match track {
+                        RemoteTrack::Audio(track) => {
+                            let sample_rate = 48000;
+                            let channels = 2;
+
+                           //NativeAudioStream::new(track, sample_rate, ) track.
+                        },
+                        _ => {},
+                    }
+
+                }
+                _ => {}
+            }
+        }
     }
 }
